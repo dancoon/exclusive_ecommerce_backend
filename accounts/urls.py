3def972a4aea6f63b6158a3122f0b8f3f3c4346e
dancoon <dancun.ian@students.jkuat.ac.ke>
2024-02-03 04:05:34 +0300

    fix: :bug: Fix endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from .views import CustomTokenObtainPairView, CustomTokenRefreshView, CustomToke
 
 urlpatterns = [
     path("auth/", include("djoser.urls")),
-    path("auth/jwt/create", view=CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("auth/jwt/refresh", view=CustomTokenRefreshView.as_view(), name="token_refresh"),
-    path("auth/jwt/verify", view=CustomTokenVerifyView.as_view(), name="token_verify"),
-    path("auth/logout", view=LogoutView.as_view(), name="auth_logout"),
+    path("auth/jwt/create/", view=CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/jwt/refresh/", view=CustomTokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/jwt/verify/", view=CustomTokenVerifyView.as_view(), name="token_verify"),
+    path("auth/logout/", view=LogoutView.as_view(), name="auth_logout"),
 ]
